345bbac01945800360698e367ffb79abc99339e3
Leandro <leandrocarta@gmail.com>
2023-06-28 09:46:35 -0300

    Commit se agrega Proyecto

---
+++ app/Http/Controllers/EmprendedorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Emprendedores;
+use Illuminate\Http\Request;
+
+class EmprendedorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Emprendedores $emprendedores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Emprendedores $emprendedores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Emprendedores $emprendedores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Emprendedores $emprendedores)
+    {
+        //
+    }
+}
